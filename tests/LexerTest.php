d4c79aadd90cc3f1fd3b7a88c29ba96c126f3d11
Emile Silas SARE <emile.silas@gmail.com>
2022-04-02 19:50:11 +0100

    lexer tests added

---
+++ tests/LexerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright (c) 2021-present, Emile Silas Sare
+ *
+ * This file is part of Blate package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Blate\Tests;
+
+use Blate\Lexer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+final class LexerTest extends TestCase
+{
+	/**
+	 * @throws \Blate\Exceptions\BlateException
+	 */
+	public function testTokenize(): void
+	{
+		$dir           = BLATE_TEST_TEMPLATES_DIR . '/lexer';
+		$template_file = $dir . '/template.blate';
+		$tokens_file   = $dir . '/tokens.json';
+		$template      = \file_get_contents($template_file);
+
+		$lexer = new Lexer($template);
+
+		$tokens = $lexer->tokenize();
+		$output = \json_encode($tokens, \JSON_THROW_ON_ERROR | \JSON_PRETTY_PRINT);
+
+		if (\file_exists($tokens_file)) {
+			$expected = \file_get_contents($tokens_file);
+			static::assertSame($expected, $output);
+		} else {
+			\file_put_contents($tokens_file, $output);
+		}
+	}
+}
